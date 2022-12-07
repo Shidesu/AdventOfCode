bf195951f7a0e39b374fd1b0a5ef0041f07dcda8
Kao <floconetflanelle@gmail.com>
2022-12-07 08:43:44 +0100

    day06

---
+++ AdventOfCode/2022/Day06.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCodeSupport;
+
+namespace AdventOfCodeDay1._2022;
+
+public class Day06 : AdventBase
+{
+    protected override void InternalPart1()
+    {
+        var result = InputText
+                .Select((x, i) =>
+                    i < InputText.Length - 4
+                        ? (span: InputText.AsMemory(i, 4), startingSequence: i + 4)
+                        : (span: InputText.AsMemory(i), startingSequence: i + 4))
+                .First(x => x.span.Span.ToArray().Distinct().Count() == 4)
+            ;
+
+        Console.WriteLine(result.startingSequence);
+    }
+
+    protected override void InternalPart2()
+    {
+        var result = InputText
+                .Select((x, i) =>
+                    i < InputText.Length - 14
+                        ? (span: InputText.AsMemory(i, 14), startingSequence: i + 14)
+                        : (span: InputText.AsMemory(i), startingSequence: i + 14))
+                .First(x => x.span.Span.ToArray().Distinct().Count() == 14)
+            ;
+
+        Console.WriteLine(result.startingSequence);
+    }
+}
